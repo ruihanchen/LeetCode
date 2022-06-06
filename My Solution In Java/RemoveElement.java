3631c30550252a08d1d1247c3078eef89bb075d5
Ruihan Chen <ruihanchen@berkeley.edu>
2022-06-06 19:00:01 -0400

    Create RemoveElement.java

---
+++ My Solution In Java/RemoveElement.java
@@ -0,0 +1,11 @@
+public int removeElement(int[] nums, int val) {
+    int fast = 0;
+    int slow = 0;
+    while (fast < nums.length) {
+        if (nums[fast] != val) {
+            nums[slow++] = nums[fast];
+        }
+        fast++;
+    }
+    return slow;
+}
