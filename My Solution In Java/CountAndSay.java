3a61330a9f30fcc60c6f96d284c495290e2b1a13
Ruihan Chen <ruihanchen@berkeley.edu>
2022-07-01 15:36:47 -0400

    Create CountAndSay.java

---
+++ My Solution In Java/CountAndSay.java
@@ -0,0 +1,37 @@
+//Solution 1
+public String countAndSay(int n) {
+
+    if (n == 1) {
+        return "1";
+    }
+
+    String last = countAndSay(n - 1);
+
+    return getNextString(last);
+
+}
+
+private String getNextString(String last) {
+
+    if (last.length() == 0) {
+        return "";
+    }
+
+    int num = getRepeatNum(last);
+
+    return num + "" + last.charAt(0) + getNextString(last.substring(num));
+}
+
+private int getRepeatNum(String string) {
+    int count = 1;
+    char same = string.charAt(0);
+    for (int i = 1; i < string.length(); i++) {
+        if (same == string.charAt(i)) {
+            count++;
+        } else {
+            break;
+        }
+    }
+    return count;
+}
+//End
